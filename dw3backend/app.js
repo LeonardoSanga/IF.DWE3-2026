2b92aa2522f9333c58569dc0a578334e509ebe45
Leonardo Minguini Sanga <leonardo.sanga@aluno.ifsp.edu.br>
2026-08-26 07:30:10 -0300

    Subindo correções no projeto

--- dw3backend/app.js
+++ dw3backend/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const router = require('./routes/routes');
+const router = require('./routes/router');
 
 const app = express(); 
 const port = 40000;
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(express.json());
--- dw3backend/routes/router.js
+++ dw3backend/routes/router.js
@@ -1,6 +1,6 @@
-//-- routes/routes.js $`
 const express = require("express");
 const routerApp = express.Router();
+const appLogin = require("../apps/login/controller/ctlLogin");
 
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
@@ -10,7 +10,9 @@ routerApp.get("/", (req, res) => {
     res.send("Olá mundo!");
 });
 
-//@ Rotas de Alunos
-//@ Rotas de Cursos
+//Rotas de Alunos
+//Rotas de Cursos
 // Rota Login
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
 module.exports = routerApp;
